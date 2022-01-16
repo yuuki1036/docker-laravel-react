4f4d9968db46d523b41c4e7cb965a6392f416b3b
yuuki1036 <yuuki1036@gmail.com>
2022-01-16 15:05:11 +0900

    add api post

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["middleware" => "api"], function() {
+  Route::get("posts", "App\Http\Controllers\Api\PostController@index");
 });
